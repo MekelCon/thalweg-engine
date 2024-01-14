40145f47d6cdc2dd3f12a13ce9f91a21e4c7693a
Thibault <klein.thib@gmail.com>
2024-01-14 14:38:44 +0100

    working transition based on grayscale image

--- src/main/java/fr/thalweg/engine/system/rendering/RenderingSystem.java
+++ src/main/java/fr/thalweg/engine/system/rendering/RenderingSystem.java
@@ -5,13 +5,17 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.SortedIteratingSystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import fr.thalweg.engine.component.SpriteComponent;
 import fr.thalweg.engine.component.ZIndexComponent;
@@ -25,6 +29,16 @@ public class RenderingSystem extends SortedIteratingSystem {
     private final SpriteBatch batch;
     private final FrameBuffer worldBuffer;
     private final Viewport viewport;
+    private final ShaderProgram worldShader;
+
+
+    // Transition rendering
+    private final FrameBuffer transitionBuffer;
+    private final TextureRegion white;
+    private final TextureRegion transition;
+    private final ShaderProgram transitionShader;
+    private float sumDeltaTime;
+    private float transitionPercent;
 
 
     public RenderingSystem(WorldData world, SpriteBatch batch, Viewport viewport) {
@@ -45,20 +59,55 @@ public class RenderingSystem extends SortedIteratingSystem {
 
         this.batch = batch;
         this.viewport = viewport;
+
+        this.transitionBuffer = new FrameBuffer(
+                Pixmap.Format.RGBA8888,
+                world.getWidth(),
+                world.getHeight(),
+                false);
+        transitionBuffer.getColorBufferTexture().bind(1);
+        Gdx.gl.glActiveTexture(GL20.GL_TEXTURE0);
+        var vertexShader = Gdx.files.internal("shader/vertex.glsl").readString();
+        var fragmentShader = Gdx.files.internal("shader/fragment.glsl").readString();
+        worldShader = new ShaderProgram(vertexShader, fragmentShader);
+        if (!worldShader.isCompiled()) {
+            Gdx.app.error("Shader", worldShader.getLog());
+            Gdx.app.exit();
+        }
+        worldShader.bind();
+        worldShader.setUniformi("u_texture", 0);
+        worldShader.setUniformi("u_mask", 1);
+
+
+        vertexShader = Gdx.files.internal("shader/vertex.glsl").readString();
+        fragmentShader = Gdx.files.internal("shader/fragment_transition.glsl").readString();
+        transitionShader = new ShaderProgram(vertexShader, fragmentShader);
+        if (!transitionShader.isCompiled()) {
+            Gdx.app.error("Shader", transitionShader.getLog());
+            Gdx.app.exit();
+        }
+        transitionShader.bind();
+        transitionShader.setUniformi("u_texture", 0);
+        transitionShader.setUniformi("u_mask", 1);
+        this.white = new TextureRegion(new Texture(
+                Gdx.files.internal("quercus/transition/white.png")));
+        this.transition = new TextureRegion(new Texture(
+                Gdx.files.internal("quercus/transition/screen_transition_2.png")));
     }
 
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
+        ScreenUtils.clear(Color.BLACK);
         drawWorldBuffer();
-        displayWorldBuffer();
+        drawTransitionBuffer(deltaTime);
+        renderWorldBuffer();
         renderQueue.clear();
     }
 
     private void drawWorldBuffer() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling ? GL20.GL_COVERAGE_BUFFER_BIT_NV : 0));
-
+        batch.setShader(null);
+        batch.setColor(Color.WHITE);
         worldBuffer.begin();
         batch.begin();
         for (Entity entity : renderQueue) {
@@ -69,27 +118,27 @@ public class RenderingSystem extends SortedIteratingSystem {
         worldBuffer.end();
     }
 
-    private void displayWorldBuffer() {
-        // Save current batch state
-        var originalMatrix = batch.getProjectionMatrix();
-        var originalShader = batch.getShader();
-        int originalBlendSrcFunc = batch.getBlendSrcFunc();
-        int originalBlendDstFunc = batch.getBlendDstFunc();
+    private void drawTransitionBuffer(float deltaTime) {
+        if (transitionPercent < 1) {
+            sumDeltaTime += deltaTime;
+            transitionPercent = sumDeltaTime / 0.5f;
+            transitionBuffer.begin();
+            batch.setColor(transitionPercent, transitionPercent, transitionPercent, 1);
+            batch.draw(white, 0, 0, 256, 144);
+            batch.setColor(Color.WHITE);
+            batch.setShader(transitionShader);
+            batch.draw(transition, 0, 0, 256, 144);
+            batch.flush();
+            transitionBuffer.end();
+        }
+    }
 
+    private void renderWorldBuffer() {
         viewport.apply(true);
-
-        batch.disableBlending();
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT | (Gdx.graphics.getBufferFormat().coverageSampling ? GL20.GL_COVERAGE_BUFFER_BIT_NV : 0));
         batch.setProjectionMatrix(IDENTITY);
+        batch.setShader(worldShader);
         batch.draw(worldBuffer.getColorBufferTexture(), -1, -1, 2, 2);
         batch.end();
-        batch.enableBlending();
-
-        // Restore batch state
-        batch.setShader(originalShader);
-        batch.setProjectionMatrix(originalMatrix);
-        batch.setBlendFunction(originalBlendSrcFunc, originalBlendDstFunc);
     }
 
     @Override
---
+++ src/main/resources/shader/fragment.glsl
@@ -0,0 +1,21 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform sampler2D u_texture;
+uniform sampler2D u_mask;
+
+varying vec4 v_color;
+varying vec2 v_texCoord0;
+
+void main()
+{
+    vec4 texColor = texture2D(u_texture, v_texCoord0);
+    vec4 mask = texture2D(u_mask, v_texCoord0);
+    if((mask.r + mask.g + mask.b) == 3) {
+        texColor.a = 1;
+    } else {
+        texColor.a = 0;
+    }
+    gl_FragColor = v_color * texColor;
+}
